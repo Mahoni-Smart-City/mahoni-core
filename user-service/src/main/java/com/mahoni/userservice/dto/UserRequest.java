0633a53470b1ac4c7d5449166405c8a4205a2b5c
Muhammad Fathan Muthahhari <fathanmuthahhari@gmail.com>
2023-03-17 14:40:43 +0700

    fix user request dto

--- user-service/src/main/java/com/mahoni/userservice/dto/UserRequest.java
+++ user-service/src/main/java/com/mahoni/userservice/dto/UserRequest.java
@@ -3,9 +3,11 @@ package com.mahoni.userservice.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserRequest {
   @NotNull
   private String username;
