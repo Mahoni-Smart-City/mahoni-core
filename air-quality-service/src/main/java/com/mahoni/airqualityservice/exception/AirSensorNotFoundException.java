639cdc4cf035c9aa3391495e8cafa7f9c0e1fda8
Natasya Zahra <whynatasya@gmail.com>
2023-04-05 16:16:05 +0700

    refactor airsensor controller and exception

--- air-quality-service/src/main/java/com/mahoni/airqualityservice/exception/AirSensorNotFoundException.java
+++ air-quality-service/src/main/java/com/mahoni/airqualityservice/exception/AirSensorNotFoundException.java
@@ -1,8 +1,5 @@
 package com.mahoni.airqualityservice.exception;
 
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
 public class AirSensorNotFoundException extends RuntimeException {
   public AirSensorNotFoundException(Long id) {
     super("Air Sensor with id " + id + " is not found");
