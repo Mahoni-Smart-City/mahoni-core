fe43129ac591094a5f77c79cb979004a3299cf9a
Zsaschz <fathanmuthahhari@gmail.com>
2023-05-06 22:34:20 +0700

    fix wrong file name

--- loyalty-service/src/main/java/com/mahoni/loyaltyservice/LoyaltyServiceApplication.java
+++ loyalty-service/src/main/java/com/mahoni/loyaltyservice/LoyaltyServiceApplication.java
@@ -1,15 +1,13 @@
-package com.mahoni.userservice;
+package com.mahoni.loyaltyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
-public class UserServiceApplication {
+public class LoyaltyServiceApplication {
 
   public static void main(String[] args) {
-      SpringApplication.run(UserServiceApplication.class, args);
+      SpringApplication.run(LoyaltyServiceApplication.class, args);
     }
 
 }
